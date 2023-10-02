dab074c05b48c7631d2281adfdb4371ac39bfd0b
arman <armansarg01@gmail.com>
2023-10-02 09:15:45 +0400

    added: cube root ex.

--- homework/homework_1/problem_1.cpp
+++ homework/homework_1/problem_1.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double cubeRoot(double x, double delta) {
+    if (x == 0.0) {
+        return 0.0;
+    }
+    double result = x;
+    while (abs(result * result * result - x) > delta) {
+        result = (2 * result + x / (result * result)) / 3;
+    }
+    return result;
+}
 int main() {
-
+    double cube;
+    cin >> cube;
+    cout << cubeRoot(cube, 0.0001);
 }
