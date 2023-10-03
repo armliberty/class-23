319dac44c48b95f4b8d6cccdd0be7db11a0d5d17
arman <armansarg01@gmail.com>
2023-10-03 23:17:13 +0400

    added: euclides method gcd lcd.

---
+++ homework/homework_arman/homework_1/problem_5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int euclidGcd(int num1,int num2){
+    if( num1 == num2 ){
+        return num1;
+    }
+    int b = 0;
+    if( num1 > num2 ){
+        b = num1 - num2;
+        euclidGcd( b, num2);
+    } else {
+        euclidGcd( num2, num1 ); 
+    };
+}
+int euclidLcm(int num1, int num2) {
+    return (num1 * num2) / euclidGcd( num1, num2 );
+}
+int main() {
+    int num1,num2;
+    std::cin >> num1 >> num2;
+    std::cout << "gcd is "<< euclidGcd(num1, num2) <<std::endl;
+    std::cout << "lcd is " << euclidLcm(num1, num2) <<std::endl;
+}
