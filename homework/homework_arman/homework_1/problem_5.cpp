dfb0d10427f51a35562dfcd821d681fc37dbd35a
arman <armansarg01@gmail.com>
2023-10-03 23:34:12 +0400

    added: check some cases.

--- homework/homework_arman/homework_1/problem_5.cpp
+++ homework/homework_arman/homework_1/problem_5.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
 int euclidGcd(int num1,int num2){
-    if( num1 == num2 ){
-        return num1;
-    }
-    int b = 0;
-    if( num1 > num2 ){
-        b = num1 - num2;
-        euclidGcd( b, num2);
-    } else {
-        euclidGcd( num2, num1 ); 
-    };
+	if (num1 == 0) 
+		return num2; 
+	if (num2 == 0) 
+		return num1;
+	if( num1 == num2 ){
+		return num1;
+	}
+    	int b = 0;
+    	if( num1 > num2 ){
+        	b = num1 - num2;
+        	euclidGcd( b, num2);
+    	} else {
+        	euclidGcd( num2, num1 ); 
+	};
 }
 int euclidLcm(int num1, int num2) {
     return (num1 * num2) / euclidGcd( num1, num2 );
