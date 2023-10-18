992f41dbaff8ace55aecbdd4bcf6e57832d39628
sargis-melqonyan <melqonyans500@gmail.com>
2023-10-19 00:05:10 +0400

    homework without homework3-3.5, homework4-5

--- classwork/fsm/fsm_find_all.cpp
+++ classwork/fsm/fsm_find_all.cpp
@@ -47,28 +47,6 @@ int main() {
     print(matchedIndices, "Matched index list");
 }
 
-void markFoundIndices(size_t startOffset, const std::vector<size_t>& list){
-    size_t lastElement = list[list.size() - 1];
-    size_t i,j,n;
-    // size_t newList = 
-    for(n = 0; n < startOffset; n++){std::cout<<" ";}
-    
-    for(i = 0; i < list.size(); i++){
-        for(j = 0; j < startOffset + lastElement + 1; j++){
-            if(j == list[i]){
-                std::cout<<"^";
-                break;
-            }
-            else{
-                std::cout<<" ";
-            }
-        }
-    }
-    
-    std::cout<<std::endl;
-}
-
-
 template <typename T>
 void print(T value, const std::string& msg) {
     if (!msg.empty()) {
@@ -89,22 +67,34 @@ void print(const std::vector<T>& v, const std::string& msg) {
     std::cout << "]" << std::endl;
 }
 
-
-bool areSubstringEqual(const std::string& s, size_t sub1StartIndex, size_t sub1EndIndex, size_t sub2StartIndex) {
-    return 0 == s.compare(sub1StartIndex, sub1EndIndex + 1, s, sub2StartIndex);
+void markFoundIndices(size_t startOffset, const std::vector<size_t>& indices) {
+    if (indices.empty()) {
+        return;
+    }
+    std::cout << std::string(startOffset + indices[0], ' ') << '^';
+    for (size_t i = 1; i < indices.size(); ++i) {
+        std::cout << std::string(indices[i] - indices[i - 1] - 1, ' ') << '^';
+    }
+    std::cout << std::endl;
 }
+
+bool areSubstringsEqual(const std::string& s, size_t sub1StartIndex, size_t sub1EndIndex,
+                                                size_t sub2StartIndex, size_t sub2EndIndex) {
+    return 0 == s.compare(sub1StartIndex, sub1EndIndex - sub1StartIndex + 1, s, sub2StartIndex, sub2EndIndex - sub2StartIndex + 1);
+}
+
 
 StateId computeNextState(StateId currStateId, char c, const std::string& pattern) {
     const auto finalState = pattern.size();
     if (c == pattern[currStateId] && currStateId != finalState) {
         return currStateId + 1;
     }
-    for (StateId nextState = currStateId; nextState > 0; --nextState) {
-        const auto prefixStartIndex = 0;
+    for (auto nextState = currStateId; nextState > 0; --nextState) {
         const auto prefixEndIndex = nextState - 1;
-        const auto suffixEndIndex = currStateId;
-        const auto suffixStartIndex = currStateId - (prefixEndIndex - prefixStartIndex);
-        if (areSubstringEqual(pattern, prefixStartIndex, prefixEndIndex, suffixStartIndex)) {
+        const auto suffixStartIndex = currStateId - prefixEndIndex;
+        const auto suffixEndIndex = currStateId - 1;
+        if (c == pattern[prefixEndIndex] &&
+                (prefixEndIndex == 0 || areSubstringsEqual(pattern, 0, prefixEndIndex - 1, suffixStartIndex, suffixEndIndex))) {
             return nextState;
         }
     }
@@ -128,7 +118,7 @@ std::pair<size_t, StateId> fsmCompute(const TrMatrix& trMx, StateId currState, S
     for (size_t i = startIdx; i < text.size(); ++i) {
         currState = trMx[currState][text[i]];
         if (currState == finalState) {
-            return {i - finalState + 1, currState};
+            return {i - finalState + 1, finalState};
         }
     }
     return {std::string::npos, currState};
@@ -139,9 +129,7 @@ std::vector<size_t> findAllMatches(const std::string& pattern, const std::string
     std::vector<size_t> indices;
     auto [index, currState] = fsmCompute(trMx, StateId{0}, finalState, text, 0);
     while (index != std::string::npos) {
-        if (currState == finalState) {
-            indices.push_back(index);
-        }
+        indices.push_back(index);
         std::tie(index, currState) = fsmCompute(trMx, currState, finalState, text, index + 1);
     }
     return indices;
