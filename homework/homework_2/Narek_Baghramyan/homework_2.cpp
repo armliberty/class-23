2878ff99a7d2768d9819dca5c2852f3436f8f6f5
Narekb14 <baghramyannarek300@gmail.com>
2023-10-25 10:02:41 +0400

    Add homework 1 and 2

--- homework/homework_2/Narek_Baghramyan/homework_2.cpp
+++ homework/homework_2/Narek_Baghramyan/homework_2.cpp
@@ -4,8 +4,12 @@
 #include <string>
 #include <vector>
 
+/// TODO: npos
+
+/// type alias
 using StateId = int;
 using StateIds = std::vector<StateId>;
+/// TrMatrix : char x StateIds -> StateIDs
 using TrMatrix = std::vector<StateIds>;
 
 template <typename T>
@@ -15,6 +19,7 @@ T input(std::string msg) {
     }
     T value;
     std::cin >> value;
+
     return value;
 }
 
@@ -25,25 +30,48 @@ std::string input(std::string msg) {
     }
     std::string value;
     std::getline(std::cin, value);
+
     return value;
 }
 
 template <typename T>
-void print(T value, std::string msg = "") {
+void print(T, std::string = "");
+
+TrMatrix computeTransitionMatrix(const std::string& pattern);
+std::optional<size_t> fsmFindFirstMatch(const TrMatrix& trMx, StateId finalState, const std::string& text);
+
+int main() {
+    const auto pattern = input<std::string>("Input search pattern");
+    const auto text = input<std::string>("Input text to search");
+    const auto trFunc = computeTransitionMatrix(pattern);
+    const auto finalState = pattern.size();
+    const auto optIndex = fsmFindFirstMatch(trFunc, finalState, text);
+    if (optIndex.has_value()) {
+        print((optIndex.value() + 1) - pattern.size(), "Position of the first char: ");
+        print(optIndex.value(), "Position of the last char: ");
+    } 
+    else {
+        print("Pattern has not found!");
+    }
+}
+
+template <typename T>
+void print(T value, std::string msg) {
     if (!msg.empty()) {
         std::cout << msg << ": ";
     }
     std::cout << value << std::endl;
 }
 
-bool areSubstringEqual(const std::string& s, size_t sub1StartIndex, size_t sub1EndIndex, size_t sub2StartIndex, size_t sub2EndIndex) {
-    int result = s.compare(sub1StartIndex, sub1EndIndex - sub1StartIndex + 1, s, sub2StartIndex, sub2EndIndex - sub2StartIndex + 1);
+bool areSubstringEqual(const std::string& s, size_t sub1StartIndex, size_t sub1EndIndex, size_t sub2StartIndex, size_t sub2EndIndex)
+{
+    int result = s.compare(sub1StartIndex, sub1EndIndex + 1, s, sub2StartIndex, sub2EndIndex - sub2StartIndex + 1);
     return result == 0;
 }
 
 StateId computeNextState(StateId currStateId, char c, const std::string& pattern) {
     const auto finalState = pattern.size();
-    if (c == '.' || c == pattern[currStateId]) {
+    if ((tolower(c) == pattern[currStateId] || toupper(c) == pattern[currStateId]) && currStateId != finalState) {
         return currStateId + 1;
     }
 
@@ -66,13 +94,14 @@ StateId computeNextState(StateId currStateId, char c, const std::string& pattern
 
 TrMatrix computeTransitionMatrix(const std::string& pattern) {
     const auto stateCount = 1 + pattern.size();
-    const auto alphabetSize = std::numeric_limits<char>::max();
-    TrMatrix trFunc(stateCount, StateIds(alphabetSize));
+    const auto alfabetSize = std::numeric_limits<char>::max();
+    TrMatrix trFunc(stateCount, StateIds(alfabetSize));
     for (StateId stateId = 0; stateId < stateCount; ++stateId) {
-        for (char c = 0; c < alphabetSize; ++c) {
+        for (char c = 0; c < alfabetSize; ++c) {
             trFunc[stateId][c] = computeNextState(stateId, c, pattern);
         }
     }
+
     return trFunc;
 }
 
@@ -81,29 +110,9 @@ std::optional<size_t> fsmFindFirstMatch(const TrMatrix& trMx, StateId finalState
     for (size_t i = 0; i < text.size(); ++i) {
         currState = trMx[currState][text[i]];
         if (currState == finalState) {
-            return i - finalState + 1;
+            return i;
         }
     }
 
     return std::nullopt;
-}
-
-int main() {
-    const auto pattern = input<std::string>("Input search pattern");
-    const auto text = input<std::string>("Input text to search");
-    const auto trFunc = computeTransitionMatrix(pattern);
-    const auto finalState = pattern.size();
-    size_t currentIndex = 0;
-
-    while (true) {
-        const auto optIndex = fsmFindFirstMatch(trFunc, finalState, text.substr(currentIndex));
-        if (optIndex.has_value()) {
-            currentIndex += optIndex.value() + 1;
-            print(currentIndex - pattern.size(), "Pattern found at position");
-        } else {
-            break;
-        }
-    }
-
-    return 0;
 }
